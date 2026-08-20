3f4de6e4021d21ec526b78aa12410c915fe0d756
Victor Ccanchi <victorwccv@gmail.com>
2026-08-20 18:37:42 -0500

    chore: configure Prisma with PostgreSQL

---
+++ prisma/migrations/20260821053606_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('todo', 'in-progress', 'done');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" "TaskStatus" NOT NULL DEFAULT 'todo',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
